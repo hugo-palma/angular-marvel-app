69a397c8a52a10b6fadd5efadefcc3f71e0172ac
Hugo Palma <hugodobe_@hotmail.com>
2021-02-13 18:51:05 -0600

    adding fake data json

--- src/app/characters-page/characters-page.component.ts
+++ src/app/characters-page/characters-page.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import IResult from '../types/IResults';
+import response from '../fakeData';
+import IData from '../types/IData';
 @Component({
   selector: 'app-characters-page',
   templateUrl: './characters-page.component.html',
   styleUrls: ['./characters-page.component.css']
 })
 export class CharactersPageComponent implements OnInit {
+  result: IData = JSON.parse(response.data);
 
   constructor() { }
 
